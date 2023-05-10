e28140b3ababe5bfdd64e4e83fa7f647974cb676
Tourjmen Souhail <95188930+souhailtourjmen@users.noreply.github.com>
2023-05-10 21:44:57 +0100

    refactore module Chat and  fixed bug
    module chat  #93

---
+++ logger/index.js
@@ -0,0 +1,22 @@
+
+
+const winston = require('winston');
+
+const logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.File)({
+			level: 'debug',
+			json: true,
+			filename: './debug.log',
+			handleExceptions: true
+		}),
+		new (winston.transports.Console)({
+			level: 'debug',
+			json: true,
+			handleExceptions: true
+		})
+	],
+	exitOnError: false
+});
+
+module.exports = logger;
