3ec04395946e56476076079f79963bf297f4c940
Tourjmen Souhail <95188930+souhailtourjmen@users.noreply.github.com>
2023-08-20 15:08:09 +0100

    Delete models directory

--- models/Transporteur.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-
-const transporteurSchema = mongoose.Schema({
-  transporteur: { type: String, required: true },
-});
-userSchema.plugin(uniqueValidator);
-module.exports = mongoose.model('Transporteur', userSchema);
